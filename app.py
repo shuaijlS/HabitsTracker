3f57aa8d4ad67427c18e4d49d46afd4e3dc0c4f2
shuaijl_mac <shuaijl@seas.upenn.edu>
2022-07-26 10:27:58 -0700

    Link app to MongoDB and Ready to publish

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from routes import pages
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(pages)
+    client = MongoClient(os.environ.get("MONGODB_URI"))
+    app.db = client.get_default_database()
 
     return app
 
--- routes.py
+++ routes.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, redirect, render_template, request, redirect, url_for
-from collections import defaultdict
+from flask import Blueprint, current_app, redirect, render_template, request, redirect, url_for
 import datetime
+import uuid
 
 pages=Blueprint("habits", __name__, template_folder="templates", static_folder="stsatic")
-habits = ["Test habit", "Test habit2"]
-completions = defaultdict(list)
 
+
+def today_at_midnight():
+    today = datetime.datetime.today()
+    return datetime.datetime(today.year, today.month, today.day)
 
 @pages.context_processor
 def add_calc_date_range():
-    def date_range(start: datetime.date):
+    def date_range(start: datetime.datetime):
         dates = [start + datetime.timedelta(days = diff) for diff in range(-3,4)]
         return dates
     return {"date_range":date_range}
@@ -18,26 +20,32 @@ def add_calc_date_range():
 def index():
     date_str = request.args.get("date")
     if date_str:
-        selected_date = datetime.date.fromisoformat(date_str)
+        selected_date = datetime.datetime.fromisoformat(date_str)
     else:
-        selected_date = datetime.date.today()
+        selected_date = today_at_midnight()
+    
+    habits_on_date = current_app.db.habits.find({"added": {"$lte": selected_date}})
+    completions = [habit["habit"]
+                    for habit in current_app.db.completions.find({"date":selected_date})]
     return render_template("index.html",
-     habits=habits, 
+     habits=habits_on_date, 
      title="Habit Tracker - Home", 
-     completions=completions[selected_date],
+     completions=completions,
      selected_date = selected_date)
 
 @pages.route("/add", methods=["GET", "POST"])
 def add_habit():
+    today = today_at_midnight()
     if request.method== "POST":
-        habit = request.form.get("habit")
-        habits.append(habit)
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit", selected_date = datetime.date.today())
+        current_app.db.habits.insert_one(
+            {"_id":uuid.uuid4().hex, "added":today, "name":request.form.get("habit")}
+        )
+    return render_template("add_habit.html", title="Habit Tracker - Add Habit", selected_date = today)
 
 @pages.post("/complete")
 def complete():
     date_string = request.form.get("date")
-    habit = request.form.get("habitName")
-    date = datetime.date.fromisoformat(date_string)
-    completions[date].append(habit)
+    habit = request.form.get("habitId")
+    date = datetime.datetime.fromisoformat(date_string)
+    current_app.db.completions.insert_one({"date":date, "habit":habit})
     return redirect(url_for("habits.index",date=date_string))
